79c5385ddbd5f9a7f663183285c30ce9208fad1b
Weber de Castro Lopes <weber.castro@outlook.com.br>
2021-05-09 22:05:26 -0300

    Home, Navegation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,4 +1,5 @@
 import ImgDsDark from 'assets/img/ds-dark.svg';
+import { Link } from 'react-router-dom';
 /* .tsx é o arquivo typeScript aqui do React */
 
 /* function NavBar() {    #forma normal de criar uma função#*/
@@ -7,7 +8,9 @@ const NavBar = () => {    /*semelhante a criação de função forma anonima 'ex
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
             <div className="container">
                 <nav className="my-2 my-md-0 mr-md-3">
+                    <Link to="/">
                     <img src={ImgDsDark} alt="DevSuperior" width="120" />
+                    </Link>
                 </nav>
             </div>
         </div>
