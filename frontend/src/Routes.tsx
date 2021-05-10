e93b7cd543bad59565f30920e94c8cce2d7876e2
Weber de Castro Lopes <weber.castro@outlook.com.br>
2021-05-09 21:46:11 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from 'pages/Home';
+import Dashboard from 'pages/Dashboard';
+
+// vai especificar as rotas do sistema / navegacao
+const Routes = () => {   
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/dashboard" >
+                    <Dashboard />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+
+export default Routes;
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+
+const Home = () => {   
+    return (
+        <div>
+            <h1> pagina home </h1>
+        </div>
+    );
+}
+
+
+export default Home;
